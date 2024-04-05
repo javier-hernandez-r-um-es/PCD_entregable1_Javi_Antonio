072ede22189bfb0995e039b025c1a787caf26ffa
javier-hernandez-r-um-es <javier.hernandezr@um.es>
2024-04-05 16:10:37 +0200

    Diagrama Final y implementacion

--- implementacion.py
+++ implementacion.py
@@ -7,6 +7,8 @@ class ErrorDeFormato(Exception):
 
 
 class Persona:
+    lista_dni = []
+
     def __init__(self, nombre, dni, direccion, sexo):
         self.nombre = nombre
         self.dni = dni
@@ -15,14 +17,13 @@ class Persona:
             raise ErrorDeFormato("El sexo debe ser 'V' (varón) o 'M' (mujer).")
         self.sexo = sexo
 
-        lista_dni = []
-        lista_dni.append(dni)
+        if len(dni) != 9:
+            raise ErrorDeFormato("El DNI debe tener 9 dígitos.")
 
-        for i in lista_dni:
-            if len(i)!= 9:
-                raise ErrorDeFormato("El DNI debe tener 9 dígitos.")
-            elif i  in lista_dni:
-                raise ErrorDeFormato("El DNI debe ser único.")
+        if dni in Persona.lista_dni:
+            raise ErrorDeFormato("El DNI debe ser único.")
+        
+        Persona.lista_dni.append(dni)
 
     def getNombre(self):
         return self.nombre
@@ -232,16 +233,6 @@ profesor_asociado2 = ProfesorAsociado("Elena", "34567890F", "Calle Real 30", "M"
 profesor_titular1 = ProfesorTitular("Luis", "21098765G", "Calle Mayor 2", "V", 3, departamento3, "Inteligencia Artificial")
 profesor_titular2 = ProfesorTitular("Laura", "10987654H", "Avenida Principal 11", "M", 4, departamento1, "Redes de Computadoras")
 
-persona2 = Persona("María", "12345878A", "Avenida Principal 10", "V")
-
-estudiante1 = Estudiante("Pedro", "98765432C", "Plaza España 5", "V", "2021001")
-estudiante2 = Estudiante("Ana", "65432109D", "Paseo de la Castellana 20", "V", "2021002")
-
-profesor_asociado1 = ProfesorAsociado("Carlos", "45678901E", "Avenida Libertad 15", "V", 1, departamento1)
-profesor_asociado2 = ProfesorAsociado("Elena", "34567890F", "Calle Real 30", "V", 2, departamento2)
-
-profesor_titular1 = ProfesorTitular("Luis", "21098765G", "Calle Mayor 2", "V", 3, departamento3, "Inteligencia Artificial")
-profesor_titular2 = ProfesorTitular("Laura", "10987654H", "Avenida Principal 11", "V", 4, departamento1, "Redes de Computadoras")
 
 
 asignatura1 = Asignatura("Programación", "PROG101", 6, "Informática", 1, departamento1)
