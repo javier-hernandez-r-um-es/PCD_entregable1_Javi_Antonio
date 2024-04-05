ddedf20384cd8cd292a6297e3bd3ba70f2a4fbf1
javier-hernandez-r-um-es <javier.hernandezr@um.es>
2024-04-05 15:58:23 +0200

    Implementacion

--- implementacion.py
+++ implementacion.py
@@ -11,11 +11,6 @@ class Persona:
         self.nombre = nombre
         self.dni = dni
         self.direccion = direccion
-        if sexo not in ["V","M"]:
-            raise ValueError("El valor 'sexo' solo puede tomar valores V (varón) o M (mujer)")
-        else:
-            self.sexo = sexo
-
         if sexo not in ["V", "M"]:
             raise ErrorDeFormato("El sexo debe ser 'V' (varón) o 'M' (mujer).")
         self.sexo = sexo
@@ -223,7 +218,7 @@ departamento2 = Departamento.DITEC
 departamento3 = Departamento.DIS
 
 persona1 = Persona("Juan", "12345678A", "Calle Mayor 1", "V")
-<<<<<<< HEAD
+
 persona2 = Persona("María", "87654321B", "Avenida Principal 10", "M")
 
 
@@ -236,7 +231,7 @@ profesor_asociado2 = ProfesorAsociado("Elena", "34567890F", "Calle Real 30", "M"
 
 profesor_titular1 = ProfesorTitular("Luis", "21098765G", "Calle Mayor 2", "V", 3, departamento3, "Inteligencia Artificial")
 profesor_titular2 = ProfesorTitular("Laura", "10987654H", "Avenida Principal 11", "M", 4, departamento1, "Redes de Computadoras")
-=======
+
 persona2 = Persona("María", "12345878A", "Avenida Principal 10", "V")
 
 estudiante1 = Estudiante("Pedro", "98765432C", "Plaza España 5", "V", "2021001")
@@ -247,7 +242,7 @@ profesor_asociado2 = ProfesorAsociado("Elena", "34567890F", "Calle Real 30", "V"
 
 profesor_titular1 = ProfesorTitular("Luis", "21098765G", "Calle Mayor 2", "V", 3, departamento3, "Inteligencia Artificial")
 profesor_titular2 = ProfesorTitular("Laura", "10987654H", "Avenida Principal 11", "V", 4, departamento1, "Redes de Computadoras")
->>>>>>> 421f2540c3999555a35edb4d5208ab3b1f0661ff
+
 
 asignatura1 = Asignatura("Programación", "PROG101", 6, "Informática", 1, departamento1)
 asignatura2 = Asignatura("Base de Datos", "BD101", 6, "Informática", 2, departamento2)
